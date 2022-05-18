c1ff3da4ab81b10660b43e72eb7244edd2d905ec
Ariana-Fragoso <a00829129@tec.mx>
2022-05-18 13:38:09 -0500

    x

--- códigos/edgeDetection.m
+++ códigos/edgeDetection.m
@@ -1,11 +1,12 @@
-f=imread('radiograph1.jpg');
-f=imresize(f,0.25);
-f=double(f(:,:,1));
-imshow(f,[])
+f=imread('radiograph1.jpg'); %Lee una imagen especifica de la computadora y la pasa a figure en matlab
+f=imresize(f,0.25); %El imresize cambia el tamaño de la imagen, en este caso escalandola a 0.25
+f=double(f(:,:,1)); %
+imshow(f,[]) % Muestra la imagen en una escala de grises y se especifica el rango de visualización
+
 %%
-edgex=[1,-1]
-g1=conv2(f,edgex,'same');
-imshow(g1,[-10,10]);
+edgex=[1,-1] %Crea un arreglo del borde x
+g1=conv2(f,edgex,'same'); %Devuelve una sección de la convolución, en este caso el centro. (Borde X)
+imshow(g1,[-10,10]); % Muestra una imagen en escala de grises con rango de visualización de -10 a 10 de la conv2.
 %%
 edgey=[-1 -2 -1;0,0,0;1,2,1]/8
 g2=conv2(f,edgey,'same');
