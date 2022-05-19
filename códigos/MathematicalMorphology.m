f86ef592279fdae2f12e49d330cd74894f5423ba
Ariana-Fragoso <a00829129@tec.mx>
2022-05-19 13:51:13 -0500

    ariana

--- códigos/MathematicalMorphology.m
+++ códigos/MathematicalMorphology.m
@@ -1,40 +1,70 @@
 %% Mathematical Morphology
+% codigo clase 18/05/22.
 
-% cÃ³digo clase 18/05/22. 
-f=imread('radiograph2.jpg');
-f=double(f(:,:,1));
+%Lee una imagen especifica de la computadora y la pasa a figure en matlab.
+%Convierte la variable a doble precisión y la funcion imresize cambia el
+%tamaño de la imagen, en este caso escalandola a 0.25, despues se
+%inicializa la figura mostrando una imagen en una escala de grises y se
+%especifica el rango de visualizacion 
+f=imread('radiograph2.jpg'); 
+f=double(f(:,:,1)); 
 f=f/max(max(f));
 f=imresize(f,0.25);
-figure(1)
+figure(1) 
 imshow(f,[]);
-
+title('Radiograph') 
 %% Dilatation
 
-se = strel('disk',5);
+%El strel crea un elemento estructurante (SE) el cual asume valores binarios y
+%puede ser en 2D como en este caso. Con la funcion imdilate dilata la
+%imagen en una escala de grises, se puede ver menos visible la imagen
+%conforme subas el SE.
+se = strel('disk',8); 
 BW2 = imdilate(f,se);
-imshow(BW2), title('dilated')
+imshow(BW2)
+title('Dilatation')
 % Use different disk size
 %% Erosion
 
-se = strel('disk',5);
+%El strel crea un elemento estructurante (SE) el cual asume valores binarios y
+%puede ser en 2D como en este caso. Con la funcion imerode erosiona la
+%imagen en una escala de grises. En este caso se hace un subplot para que
+%muestre la imagen erosionada junto con la imagen de la funcion imopen 
+se = strel('disk',10); %tenía 5
 BW3 = imerode(f,se);
-imshow(BW3), title('Eroded')
+%imshow(BW3), title('Eroded')
 % Use different disk size
-imshowpair(BW2,BW3,'montage')
+%imshowpair(BW2,BW3,'montage')
+%title('eroded')
+
+subplot(1,2,1),imshow(BW3),title('Eroded');hold  on
+subplot(1,2,2),imshowpair(BW2,BW3,'montage'),title('Montage');hold off
 %% Opening
 
-se = strel('disk',7);
+%El strel crea un elemento estructurante (SE) el cual asume valores binarios y
+%puede ser en 2D como en este caso. La funcion imopen consiste en una
+%erosión seguida de una dilatación utilizando SE, despues se muestra la
+%imagen erosionada y dilatada con un titulo.
+se = strel('disk',12); %tenía 7
 BW2 = imopen(f,se);
-imshow(BW2), title('Opening')
+imshow(BW2)
+title('Opening')
 % Use different disk size
 %% Closing
 
-se = strel('disk',7);
+%El strel crea un elemento estructurante (SE) el cual asume valores binarios y
+%puede ser en 2D como en este caso. La funcion imclose es lo contrario de
+%imopen ya que consiste en primero la dilatacion seguida de una erosion
+%utilizando SE, despues se  muestra la imagen dilatada y erosionada con un titulo.
+se = strel('disk',15); %tenía 7 
 BW2 = imclose(f,se);
-imshow(BW2), title('Closing')
+imshow(BW2)
+title('Closing')
 % Use different disk size
 %% Gradient
 
+%El strel crea un elemento estructurante (SE) el cual asume valores binarios y
+%puede ser en 2D como en este caso. 
 se = strel('disk',1);
 BW1 = imdilate(f,se) - imerode(f,se);
 imshow(BW1), title('Gradient')
@@ -43,8 +73,9 @@ imshow(BW1), title('Gradient')
 %% Preprocess the Image The Rice Matlab Example
 % Read an image into the workspace.
 
-I = imread('rice.png');
+I = imread('pout.tif');
 imshow(I)
+title('Pout image')
 %% 
 % The background illumination is brighter in the center of the image than at 
 % the bottom. Preprocess the image to make the background illumination more uniform.
@@ -60,6 +91,7 @@ se = strel('disk',15)
 
 background = imopen(I,se);
 imshow(background)
+title('Opening')
 %% 
 % Subtract the background approximation image, |background|, from the original 
 % image, |I|, and view the resulting image. After subtracting the adjusted background 
@@ -68,19 +100,21 @@ imshow(background)
 
 I2 = I - background;
 imshow(I2)
-%% 
+title('Background')
+%%
 % Use |imadjust| to increase the contrast of the processed image |I2| by saturating 
 % 1% of the data at both low and high intensities and by stretching the intensity 
 % values to fill the |uint8| dynamic range.
 
-I3 = imadjust(I2);
+I3 = imadjust(I2)
 imshow(I3)
+title('Foto ajustada')
 %% 
 % Note that the prior two steps could be replaced by a single step using |imtophat| 
 % which first calculates the morphological opening and then subtracts it from 
 % the original image.
 % 
-% |I2 = imtophat(I,strel('disk',15));|
+% I2 = imtophat(I,strel('disk',15));|
 %% 
 % Create a binary version of the processed image so you can use toolbox functions 
 % for analysis. Use the |imbinarize| function to convert the grayscale image into 
@@ -88,8 +122,9 @@ imshow(I3)
 % function.
 
 bw = imbinarize(I3);
-bw = bwareaopen(bw,50);
+bw = bwareaopen(bw,10);
 imshow(bw)
+title('Binary image')
 
 % Use different size of the structural element
 
@@ -97,7 +132,7 @@ imshow(bw)
 % Read a 2-D grayscale image into the workspace. Display the image. Objects 
 % of interest are dark threads against a light background.
 
-I = imread('threads.png');
+I = imread('Skeletonize.jpg');
 imshow(I)
 %% 
 % Skeletonization requires a binary image in which foreground pixels are |1| 
